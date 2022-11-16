02ff0e155c83901e2041430041c6941e36702b36
Admin <Admin@TEB123014>
2022-11-16 12:19:27 +0100

    Add project files.

---
+++ Uppgift P1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många sidor kommer tärningarna ha?: "); int sidor = int.Parse(Console.ReadLine());
+            Console.Write("Hur många tärnignar kommer slås?: "); int antal = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int[] tal = new int[antal];
+
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Random random = new Random();
+                tal[i] = random.Next(1, sidor+1);
+                Console.WriteLine($"Tärningen slog {tal[i]}.");
+            }
+
+            Console.WriteLine();
+            int Summa = tal.Sum();
+            float Medelvärde = Summa / antal;
+
+            Console.WriteLine($"Du slog totalt {Summa} och fick ett medelvärde på {Medelvärde}.");
+        }
+    }
+}
